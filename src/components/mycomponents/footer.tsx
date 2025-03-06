6fc39aece489d9e0e2616de7c9ff96a9c04b1731
Muhammad Nauman <nauman33183@gmail.com>
2025-03-06 12:34:39 +0500

    footer links

--- src/components/mycomponents/footer.tsx
+++ src/components/mycomponents/footer.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import { Facebook, Twitter, Linkedin, Github } from "lucide-react";
+import { Facebook, Linkedin, Github } from "lucide-react";
+import { FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -27,16 +28,16 @@ const Footer = () => {
 
         {/* Social Media Icons */}
         <div className="flex space-x-4 mt-4 md:mt-0">
-          <a href="https://github.com" target="_blank" className="hover:text-red-500 transition duration-300">
+          <a href="https://github.com/nauman331" target="_blank" className="hover:text-red-500 transition duration-300">
             <Github size={24} />
           </a>
-          <a href="https://linkedin.com" target="_blank" className="hover:text-red-500 transition duration-300">
+          <a href="https://www.linkedin.com/in/muhammad-nauman-72a2b2298?utm_source=share&utm_compaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" className="hover:text-red-500 transition duration-300">
             <Linkedin size={24} />
           </a>
-          <a href="https://twitter.com" target="_blank" className="hover:text-red-500 transition duration-300">
-            <Twitter size={24} />
+          <a href="https://www.tiktok.com/@naumansoftwareengineer?_t=ZS-8uRvIidEiee&_r=1" target="_blank" className="hover:text-red-500 transition duration-300">
+            <FaTiktok size={24} />
           </a>
-          <a href="https://facebook.com" target="_blank" className="hover:text-red-500 transition duration-300">
+          <a href="https://www.facebook.com/profile.php?id=100091209582055&mibextid=dZk1I5icssMIZk4L" target="_blank" className="hover:text-red-500 transition duration-300">
             <Facebook size={24} />
           </a>
         </div>
