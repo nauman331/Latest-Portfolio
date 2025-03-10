6e1d8c2fae156c9669d9f75a3d97ff75d705dcb6
Muhammad Nauman <nauman33183@gmail.com>
2025-03-10 13:34:50 +0500

    redux-precistor

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./themeSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; 
+
+const themePersistConfig = {
+  key: "theme",
+  storage,
+};
+
+const persistedThemeReducer = persistReducer(themePersistConfig, themeReducer);
+
 
 export const store = configureStore({
   reducer: {
-    theme: themeReducer,
+    theme: persistedThemeReducer,
   },
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
