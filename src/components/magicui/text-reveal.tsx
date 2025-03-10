2aeb36d5067647eee2642079a2b540f7b4ad68ce
Muhammad Nauman <nauman33183@gmail.com>
2025-03-10 11:40:45 +0500

    skills section compeleted

--- src/components/magicui/text-reveal.tsx
+++ src/components/magicui/text-reveal.tsx
@@ -1,5 +1,6 @@
 "use client";
-
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
 import { ComponentPropsWithoutRef, FC, ReactNode, useRef } from "react";
 
@@ -10,6 +11,7 @@ export interface TextRevealProps extends ComponentPropsWithoutRef<"div"> {
 }
 
 export const TextReveal: FC<TextRevealProps> = ({ text, className }) => {
+
   const targetRef = useRef<HTMLDivElement | null>(null);
 
   const { scrollYProgress } = useScroll({
@@ -17,11 +19,11 @@ export const TextReveal: FC<TextRevealProps> = ({ text, className }) => {
   });
 
   const words = text.split(" "); // Splitting by space to keep words intact
-
+  const theme = useSelector((state: RootState) => state.theme.theme);
   return (
     <div ref={targetRef} className={cn("relative z-0 h-auto", className)}>
       <div className="sticky top-0 mx-auto flex h-[50%] max-w-4xl items-center bg-transparent p-5">
-        <p className="flex flex-wrap gap-2 text-lg md:text-2xl font-bold text-black/20 dark:text-white/20 md:p-8 lg:p-10">
+        <p className={`flex flex-wrap gap-2 text-lg md:text-2xl font-bold ${theme === "dark" ? "text-white/20" : "text-black/20"} md:p-8 lg:p-10`}>
           {words.map((word, i) => {
             const start = (words.length - i - 1) / words.length; // Reverse the start positions
             const end = start + 1 / words.length;
@@ -44,11 +46,12 @@ interface WordProps {
 }
 
 const Word: FC<WordProps> = ({ children, progress, range }) => {
+  const theme = useSelector((state: RootState) => state.theme.theme);
   const opacity = useTransform(progress, range, [1, 0]); // Ensure words fade in the correct order
   return (
     <span className="relative">
       <span className="absolute opacity-30">{children}</span>
-      <motion.span style={{ opacity }} className="text-black dark:text-white">
+      <motion.span style={{ opacity }} className={theme === "dark" ? "text-white" : "text-black"}>
         {children}
       </motion.span>
     </span>
